5c0e5cba1795cfdc2b88212ce21faeab89253731
SamChichester <lnsschi1225@gmail.com>
2024-12-12 00:21:42 -0500

    Set up index and about pages and Layout component.

---
+++ frontend/pages/components/layout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC <LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Spotify Rewrapped</title>
+      </Head>
+
+      <header>
+        <div>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Sam Chichester. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
