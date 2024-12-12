a46c898ac450be794ed707dd2bca27b1ccd14fec
SamChichester <lnsschi1225@gmail.com>
2024-12-12 15:09:14 -0500

    Adjusted about.test.tsx with some preliminary structure.

--- frontend/__test__/about.test.tsx
+++ frontend/__test__/about.test.tsx
@@ -1,13 +1,16 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import About from '@/pages/about'
+import '@testing-library/jest-dom';
+import { render, screen, cleanup } from '@testing-library/react';
+import About from '@/pages/about';
 
-describe('About', () => {
-  it('renders a heading', () => {
-    render(<About />)
- 
-    const heading = screen.getByRole('heading', { level: 1 })
- 
-    expect(heading).toBeInTheDocument()
-  })
+afterEach(() => {
+  cleanup();
 })
+
+describe('About Component', () => {
+  render(<About />);
+
+  test('Renders heading.', () => {
+    const heading = screen.getByRole('heading', { level: 1 });
+    expect(heading).toBeInTheDocument();
+  });
+});
